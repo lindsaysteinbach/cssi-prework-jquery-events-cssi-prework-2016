c783f47f05025fb660a93d88bb2519a7a24072d0
lindsaysteinbach <lsteinbach@google.com>
2016-05-07 12:51:37 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
 
+function getIt () {
+  alert ("Hey!");
+}
+
+function frameIt () {
+  $(this).addClass("tasty");
+}
+
+function pressIt () {
+  if (event.keyCode==71) {
+    alert ("You typed g");
+  }
+}
+
+function submitIt () {
+  alert ("your form is going to be submitted now")
+  return;
+}
+
 $(document).ready(function(){
 
-// call functions here
-
+$("p").click(getIt);
+$("img").load(frameIt);
+$("#typing").keydown(pressIt);
+$("form").submit(submitIt);
 });
